9e77db6300cee54a93e5d987f8a9a45b65f89dbf
SpringSunYY <3217778348@qq.com>
2025-05-05 13:54:37 +0800

    修改用户表结构，新增年龄身高体重

--- scientific-admin/src/main/java/com/lz/web/controller/system/SysLoginController.java
+++ scientific-admin/src/main/java/com/lz/web/controller/system/SysLoginController.java
@@ -2,6 +2,7 @@ package com.lz.web.controller.system;
 
 import java.util.List;
 import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,12 +22,11 @@ import com.lz.system.service.ISysMenuService;
 
 /**
  * 登录验证
- * 
+ *
  * @author ruoyi
  */
 @RestController
-public class SysLoginController
-{
+public class SysLoginController {
     @Autowired
     private SysLoginService loginService;
 
@@ -41,13 +41,12 @@ public class SysLoginController
 
     /**
      * 登录方法
-     * 
+     *
      * @param loginBody 登录信息
      * @return 结果
      */
     @PostMapping("/login")
-    public AjaxResult login(@RequestBody LoginBody loginBody)
-    {
+    public AjaxResult login(@RequestBody LoginBody loginBody) {
         AjaxResult ajax = AjaxResult.success();
         // 生成令牌
         String token = loginService.login(loginBody.getUsername(), loginBody.getPassword(), loginBody.getCode(),
@@ -58,20 +57,18 @@ public class SysLoginController
 
     /**
      * 获取用户信息
-     * 
+     *
      * @return 用户信息
      */
     @GetMapping("getInfo")
-    public AjaxResult getInfo()
-    {
+    public AjaxResult getInfo() {
         LoginUser loginUser = SecurityUtils.getLoginUser();
         SysUser user = loginUser.getUser();
         // 角色集合
         Set<String> roles = permissionService.getRolePermission(user);
         // 权限集合
         Set<String> permissions = permissionService.getMenuPermission(user);
-        if (!loginUser.getPermissions().equals(permissions))
-        {
+        if (!loginUser.getPermissions().equals(permissions)) {
             loginUser.setPermissions(permissions);
             tokenService.refreshToken(loginUser);
         }
@@ -84,12 +81,11 @@ public class SysLoginController
 
     /**
      * 获取路由信息
-     * 
+     *
      * @return 路由信息
      */
     @GetMapping("getRouters")
-    public AjaxResult getRouters()
-    {
+    public AjaxResult getRouters() {
         Long userId = SecurityUtils.getUserId();
         List<SysMenu> menus = menuService.selectMenuTreeByUserId(userId);
         return AjaxResult.success(menuService.buildMenus(menus));
--- scientific-ui/src/views/system/user/profile/userInfo.vue
+++ scientific-ui/src/views/system/user/profile/userInfo.vue
@@ -1,19 +1,28 @@
 <template>
   <el-form ref="form" :model="form" :rules="rules" label-width="80px">
     <el-form-item label="用户昵称" prop="nickName">
-      <el-input v-model="form.nickName" maxlength="30" />
-    </el-form-item> 
+      <el-input v-model="form.nickName" maxlength="30"/>
+    </el-form-item>
     <el-form-item label="手机号码" prop="phonenumber">
-      <el-input v-model="form.phonenumber" maxlength="11" />
+      <el-input v-model="form.phonenumber" maxlength="11"/>
     </el-form-item>
     <el-form-item label="邮箱" prop="email">
-      <el-input v-model="form.email" maxlength="50" />
+      <el-input v-model="form.email" maxlength="50"/>
     </el-form-item>
     <el-form-item label="性别">
       <el-radio-group v-model="form.sex">
         <el-radio label="0">男</el-radio>
         <el-radio label="1">女</el-radio>
       </el-radio-group>
+    </el-form-item>
+    <el-form-item label="年龄">
+      <el-input-number :min="0" :max="120" v-model="form.age" placeholder="请输入年龄" maxlength="30"/>
+    </el-form-item>
+    <el-form-item label="体重(KG)">
+      <el-input-number :min="0" :max="200" :percision="2" v-model="form.weight" placeholder="请输入体重"/>
+    </el-form-item>
+    <el-form-item label="身高(CM)">
+      <el-input-number :min="0" :max="200" :percision="2" v-model="form.height" placeholder="请输入身高"/>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" size="mini" @click="submit">保存</el-button>
@@ -23,7 +32,7 @@
 </template>
 
 <script>
-import { updateUserProfile } from "@/api/system/user";
+import { updateUserProfile } from '@/api/system/user'
 
 export default {
   props: {
@@ -37,32 +46,49 @@ export default {
       // 表单校验
       rules: {
         nickName: [
-          { required: true, message: "用户昵称不能为空", trigger: "blur" }
+          { required: true, message: '用户昵称不能为空', trigger: 'blur' }
         ],
         email: [
-          { required: true, message: "邮箱地址不能为空", trigger: "blur" },
+          { required: true, message: '邮箱地址不能为空', trigger: 'blur' },
           {
-            type: "email",
-            message: "请输入正确的邮箱地址",
-            trigger: ["blur", "change"]
+            type: 'email',
+            message: '请输入正确的邮箱地址',
+            trigger: ['blur', 'change']
           }
         ],
         phonenumber: [
-          { required: true, message: "手机号码不能为空", trigger: "blur" },
+          { required: true, message: '手机号码不能为空', trigger: 'blur' },
           {
             pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,
-            message: "请输入正确的手机号码",
-            trigger: "blur"
+            message: '请输入正确的手机号码',
+            trigger: 'blur'
           }
+        ],
+        age: [
+          { required: true, message: '年龄不能为空', trigger: 'blur' }
+        ],
+        weight: [
+          { required: true, message: '体重不能为空', trigger: 'blur' }
+        ],
+        height: [
+          { required: true, message: '身高不能为空', trigger: 'blur' }
         ]
       }
-    };
+    }
   },
   watch: {
     user: {
       handler(user) {
         if (user) {
-          this.form = { nickName: user.nickName, phonenumber: user.phonenumber, email: user.email, sex: user.sex };
+          this.form = {
+            nickName: user.nickName,
+            phonenumber: user.phonenumber,
+            email: user.email,
+            sex: user.sex,
+            age: user.age,
+            weight: user.weight,
+            height: user.height
+          }
         }
       },
       immediate: true
@@ -70,19 +96,19 @@ export default {
   },
   methods: {
     submit() {
-      this.$refs["form"].validate(valid => {
+      this.$refs['form'].validate(valid => {
         if (valid) {
           updateUserProfile(this.form).then(response => {
-            this.$modal.msgSuccess("修改成功");
-            this.user.phonenumber = this.form.phonenumber;
-            this.user.email = this.form.email;
-          });
+            this.$modal.msgSuccess('修改成功')
+            this.user.phonenumber = this.form.phonenumber
+            this.user.email = this.form.email
+          })
         }
-      });
+      })
     },
     close() {
-      this.$tab.closePage();
+      this.$tab.closePage()
     }
   }
-};
+}
 </script>
